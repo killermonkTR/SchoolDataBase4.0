8f392427ed4d19a5c59f50cf7ff500d2a0a2a361
KillermonkTR <KillermonkTR@users.noreply.github.com>
2022-04-16 19:36:34 +0300

    Add project files.

---
+++ SchoolDataBase4.0/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolDataBase4._0
+{
+    public partial class DataBase : Form
+    {
+        public DataBase()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SchoolDataBase4.0/Form3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SchoolDataBase4._0
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == textBox1.Text)
+            {
+
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\diren\source\repos\SchoolDataBase4.0\SchoolDataBase4.0\Database1.mdf;Integrated Security=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update login set password='" + textBox2.Text + "'where uid='" + textBox1.Text + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Password Changed..");
+            }
+            else
+            {
+                MessageBox.Show("Passwords not matching");
+            }
+        }
+    }
+}
